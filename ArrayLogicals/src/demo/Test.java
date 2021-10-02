ff397c303ebf54580b080bcd567ab15a3eda1ba9
Manojamale <manojamale01@gmail.com>
2021-10-01 22:49:17 -0700

    Test Updated

--- ArrayLogicals/src/demo/Test.java
+++ ArrayLogicals/src/demo/Test.java
@@ -7,6 +7,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			System.out.println("main method start");
 
 		Integer maxNumber = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
                 .max(Comparator.comparing(Integer::valueOf))
